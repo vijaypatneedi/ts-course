f75ebd152ea6a768ced5ab5314ed55c4b3adf34e
vijay patneedi <vpatneedi@deloitte.com>
2023-01-08 21:07:39 +0530

    add: layout for content area, sidebar area

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React, {FC, ReactElement } from 'react';
+import React, { FC, ReactElement } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './theme/customTheme';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-
-const App:FC=():ReactElement=> {
+const App: FC = (): ReactElement => {
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
